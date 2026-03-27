e52e1d32f9e740b901c20ed17ea86f98823ba06f
Aditya Idnani <adityaidnani2@gmail.com>
2026-03-27 15:22:12 +0530

    Removed wrong local path files

--- frontend/Developer/Projects/AI Resume Analyser/.next/dev/static/chunks/Developer_Projects_AI Resume Analyser_src_app_favicon_ico_mjs_a620df93._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/1e1f2_next_dist_90858bf0._.js"
-],
-    source: "dynamic"
-});
--- frontend/Developer/Projects/AI Resume Analyser/.next/dev/static/chunks/Developer_Projects_AI Resume Analyser_src_app_layout_tsx_9ff25bd0._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__e7b2d6c4._.css",
-  "static/chunks/Developer_Projects_AI Resume Analyser_14865722._.js"
-],
-    source: "dynamic"
-});
